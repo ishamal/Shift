ec81c2d232da0e95b9dfc0fce909b94fcaa03d3a
isharaWaidyasekara <isharaw@ceylonsolutions.com>
2021-08-16 20:40:38 +0530

    testcase

---
+++ app/src/androidTest/java/com/temper/myapplication/repositories/ShiftRepositoryTest.kt
@@ -0,0 +1,34 @@
+package com.temper.myapplication.repositories
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.temper.myapplication.services.ShiftService
+import com.temper.myapplication.services.ShiftServiceClient
+import com.temper.myapplication.utils.TimeUtil
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ShiftRepositoryTest : TestCase() {
+
+    private lateinit var apiInterface : ShiftServiceClient
+    private lateinit var shiftRepository: ShiftRepository
+
+
+    @Before
+    public override fun setUp() {
+        super.setUp()
+        apiInterface = ShiftService.client
+        shiftRepository = ShiftRepository(apiInterface)
+    }
+
+    @Test
+    fun loadData_success() = runBlocking {
+        var result = shiftRepository.getJobList(TimeUtil.getCurrentDate("yyyy-MM-dd"), null)
+//        assertThat(result != null).isTrue()
+    }
+
+}
